9654fb58d843ec3cf7033ea4891e9115204a940d
Siaw Young <sy@siawyoung.com>
2019-11-02 15:37:37 +0800

    Backport code to python 2

--- iromeku/iromeku.py
+++ iromeku/iromeku.py
@@ -1,52 +1,47 @@
+from __future__ import division
 from math import sqrt
-from typing import NamedTuple, List, Any
 from PIL import Image
+from collections import namedtuple
+MYPY = False
+if MYPY:
+    from typing import NamedTuple, List, Any
 
 
-class RGB(NamedTuple):
-    """
-    RGB is the public class meant for consumption.
-    Its RGB values are integers between 0 and 255, inclusive.
-    """
-
-    r: int
-    g: int
-    b: int
+"""
+RGB is the public class meant for consumption.
+Its RGB values are integers between 0 and 255, inclusive.
+"""
+RGB = namedtuple(
+    "RGB", ["r", "g", "b"]
+)  # type: NamedTuple('RGB', [('r', int), ('g', int), ('b', int)])
 
 
-class RGBPoint(NamedTuple):
-    """
-    RGBPoint is an internal class whose values are
-    normalized to a range between 0 and 1, inclusive.
-    """
-
-    r: float
-    g: float
-    b: float
+"""
+RGBPoint is an internal class whose values are
+normalized to a range between 0 and 1, inclusive.
+"""
+RGBPoint = namedtuple(
+    "RGBPoint", ["r", "g", "b"]
+)  # type: NamedTuple('RGBPoint', [('r', float), ('g', float), ('b', float)])
 
 
-class YUVPoint(NamedTuple):
-    """
-    YUVPoint is an internal class used as part of the kernel
-    for more closely matching Euclidean proximity with colour
-    perception similarity.
-    """
-
-    y: float
-    u: float
-    v: float
+"""
+YUVPoint is an internal class used as part of the kernel
+for more closely matching Euclidean proximity with colour
+perception similarity.
+"""
+YUVPoint = namedtuple(
+    "YUVPoint", ["y", "u", "v"]
+)  # type: NamedTuple('YUVPoint', [('y', float), ('u', float), ('v', float)])
 
 
-class ImageClusters(NamedTuple):
-    """
-    ImageClusters is an internal class that serves as an intermediary
-    class for holding the results of clustering.
-    """
-
-    clusters: List[List[RGBPoint]]
+ImageClusters = namedtuple(
+    "ImageClusters", ["clusters"]
+)  # type: NamedTuple('ImageClusters', [('clusters', List[List[RGBPoint]])])
 
 
-def normalize_n_to_m(val: float, m: float, n: float) -> float:
+def normalize_n_to_m(val, m, n):
+    # type: (float, float, float) -> float
     """
     normalize_n_to_m normalizes a range of [0,m] to [0,n]
     """
@@ -54,11 +49,13 @@ def normalize_n_to_m(val: float, m: float, n: float) -> float:
     return (val / m) * n
 
 
-def round_rgb(rgb: RGBPoint) -> RGB:
+def round_rgb(rgb):
+    # type: (RGBPoint) -> RGB
     return RGB(r=round(rgb.r * 255), g=round(rgb.g * 255), b=round(rgb.b * 255))
 
 
-def rgb_to_yuv(p: RGBPoint) -> YUVPoint:
+def rgb_to_yuv(p):
+    # type: (RGBPoint) -> YUVPoint
     return YUVPoint(
         y=p.r * 0.299 + p.g * 0.587 + p.b * 0.114,
         u=p.r * -0.147 + p.g * 0.289 + p.b * 0.436,
@@ -66,7 +63,8 @@ def rgb_to_yuv(p: RGBPoint) -> YUVPoint:
     )
 
 
-def colour_distance(p: YUVPoint, q: YUVPoint) -> float:
+def colour_distance(p, q):
+    # type: (YUVPoint, YUVPoint) -> float
     """
     colour_distance returns the Euclidean distance between 2 3-tuples.
     """
@@ -74,9 +72,8 @@ def colour_distance(p: YUVPoint, q: YUVPoint) -> float:
     return sqrt((p[0] - q[0]) ** 2 + (p[1] - p[1]) ** 2 + (p[2] - q[2]) ** 2)
 
 
-def generate_clusters(
-    pixels: List[RGBPoint], distance_threshold: float
-) -> ImageClusters:
+def generate_clusters(pixels, distance_threshold):
+    # type: (List[RGBPoint], float) -> ImageClusters
     """
     generate_clusters takes in a list of RGBPoints, and a cluster threshold,
     and returns a list of clusters of RGBPoints.
@@ -86,7 +83,7 @@ def generate_clusters(
     put in a new cluster.
     """
 
-    clusters: List[List[RGBPoint]] = []
+    clusters = []  # type: List[List[RGBPoint]]
     for p in pixels:
         inserted = False
         # for each point, check if it's close enough to the first element in any existing bucket
@@ -100,7 +97,8 @@ def generate_clusters(
     return ImageClusters(clusters=clusters)
 
 
-def get_k_largest_lists(lists: List[List[Any]], k: int) -> List[List[Any]]:
+def get_k_largest_lists(lists, k):
+    # type: (List[List[Any]], int) -> List[List[Any]]
     """
     get_k_largest_lists takes in a list of lists, and returns
     the k largest lists by length, sorted in descending order by length.
@@ -111,13 +109,14 @@ def get_k_largest_lists(lists: List[List[Any]], k: int) -> List[List[Any]]:
     return sorted(lists, key=len, reverse=True)[:k]
 
 
-def mean_rgb(pixels: List[RGBPoint]) -> RGBPoint:
+def mean_rgb(pixels):
+    # type: (List[RGBPoint]) -> RGBPoint
     """
     mean_colour takes a list of points, and returns a new point whose values
     are the arithmetic mean of each band.
     """
 
-    sums: List = [0, 0, 0]
+    sums = [0, 0, 0]
     for p in pixels:
         sums[0] += p.r
         sums[1] += p.g
@@ -127,15 +126,14 @@ def mean_rgb(pixels: List[RGBPoint]) -> RGBPoint:
     )
 
 
-def load_image(path: str) -> List[RGBPoint]:
+def load_image(path):
+    # type: (str) -> List[RGBPoint]
     """
-    load_image is one of the main APIs of the library.
-
-    load_image will return an ImageClusters instance, which
-    can be used by get_dominant_colour
+    load_image takes in an image path and returns an ImageClusters instance,
+    which can be used by get_dominant_colour
     """
     im = Image.open(path, "r")
-    pixel_values: List[RGBPoint] = []
+    pixel_values = []  # type: List[RGBPoint]
     for x in im.getdata():
         # the RGB values returned by PIL are in the range [0,255]
         # which we normalize to [0,1] for later conversion to YUV
@@ -148,8 +146,12 @@ def load_image(path: str) -> List[RGBPoint]:
     return pixel_values
 
 
-def get_dominant_colour(clusters: ImageClusters) -> RGB:
+def get_dominant_colour(clusters):
+    # type: (ImageClusters) -> RGB
+    """
+    get_dominant_colour takes in an ImageClusters instance and returns
+    a dominant colour.
+    """
     largest_cluster = get_k_largest_lists(clusters.clusters, 1)
     mean = mean_rgb(largest_cluster[0])
     return round_rgb(mean)
-
--- setup.py
+++ setup.py
@@ -13,13 +13,20 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/siawyoung/iromeku",
     packages=setuptools.find_packages(),
+    package_data={"iromeku": ["py.typed"]},
     classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     install_requires=[
         'pillow',
     ],
-    python_requires='>=3.5',
 )
